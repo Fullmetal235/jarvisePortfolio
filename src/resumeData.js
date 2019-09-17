c78eba670f5786e182157827d7492ffc8e268cc3
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-09-16 22:29:31 -0500

    Setting up portfolio

---
+++ src/resumeData.js
@@ -0,0 +1,114 @@
+let resumeData = {
+    "imagebaseurl":"https://rbhatia46.github.io/",
+    "name": "Jarvise Billups-Brown",
+    "role": "Full Stack Web Developer",
+    "linkedinId":"Your LinkedIn Id",
+    "skypeid": "Your skypeid",
+    "roleDescription": "I like dabbling in various parts of web development and like to learn about new technologies, work on personal projects, or simply play games in my free time.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/jarvise-billups-brown-a41a44172/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/Fullmetal235",
+          "className":"fa fa-github"
+        },
+        {
+          "name":"twitter",
+          "url":"https://twitter.com/BrownJarvise",
+          "className":"fa fa-twitter"
+        }
+      ],
+    "aboutme":"I am currently a student in Lambda School training to become a full stack web developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"United States",
+    "website":"https://rbhatia46.github.io",
+    "education":[
+      {
+        "UniversityName":"Lambda School",
+        "specialization":"Full Stack Web Development",
+        "MonthOfPassing":"Mar",
+        "YearOfPassing":"2020"
+        
+      },
+      {
+        "UniversityName":"University of Alabama-Huntsville",
+        "specialization":"Some college",
+        "MonthOfPassing":"May",
+        "YearOfPassing":"2017"
+        },
+        {
+          "highSchoolName":"Aliceville High School",
+          "specialization":"High School Diploma",
+          "MonthOfPassing":"May",
+          "YearOfPassing":"2015" 
+        }
+    ],
+    "work":[
+      {
+        "CompanyName":"Walmart Supercenter",
+        "specialization":"Overnight Maintenance",
+        "MonthOfLeaving":"May",
+        "YearOfLeaving":"2019"
+        
+      },
+      {
+        "CompanyName":"Peco Foods",
+        "specialization":"Shipping and recieving",
+        "MonthOfLeaving":"Oct",
+        "YearOfLeaving":"2017"
+        
+      }
+    ],
+    "skillsDescription":"Your skills here",
+    "skills":[
+      {
+        "skillname":"HTML5"
+      },
+      {
+        "skillname":"CSS"
+      },
+      {
+        "skillname":"Reactjs"
+      },
+      {
+        "skillname":"Node.js"
+      }
+    ],
+    "portfolio":[
+      {
+        "name":"project1",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      },
+      {
+        "name":"project2",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/project.jpg"
+      },
+      {
+        "name":"project3",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/project2.png"
+      },
+      {
+        "name":"project4",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/phone.jpg"
+      }
+    ],
+    "testimonials":[
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      },
+      {
+        "description":"This is a sample testimonial",
+        "name":"Some technical guy"
+      }
+    ]
+  }
+  
+  export default resumeData
