a5085b2b560e1f71156075cf3565d42aa864c873
Fullmetal235 <jjbillupsbrown@gmail.com>
2020-09-10 13:54:41 -0500

    styling contact form

--- src/components/contactUs/ContactUs.js
+++ src/components/contactUs/ContactUs.js
@@ -4,6 +4,7 @@ import axios from "axios";
 function ContactUs(props) {
   const [creds, setCreds] = useState({ fullName: "", email: "", message: "" });
   const handleSubmit = () => {
+    
     axios
       .post("https://contact-backend-app.herokuapp.com/contacts", creds)
       .then((res) => {
@@ -20,26 +21,25 @@ function ContactUs(props) {
   };
   //let resumeData = props.resumeData;
   return (
-    <section id="contact">
-      <div className="row section-head">
-        <div className="ten columns">
-          <p className="lead">
+    /*<section id="contact">
+      <div classNameName="row section-head">
+        <div classNameName="ten columns">
+          <p classNameName="lead">
             Feel free to contact me for any work or suggestions below
           </p>
         </div>
       </div>
-      {/* <div className="row">
-            <aside className="eigth columns footer-widgets">
-              <div className="widget">
-                <h4>Linked in :
-                  {resumeData.linkedinId}
-                </h4>
-              </div>
-            </aside>
-          </div>*/}
+      <div classNameName="row">
+        <aside classNameName="eigth columns footer-widgets">
+          <div classNameName="widget">
+            <h4>Linked in :{resumeData.linkedinId}</h4>
+          </div>
+        </aside>
+      </div>
       <form onSubmit={handleSubmit}>
         <p>
           <input
+            className="input"
             required
             type="text"
             name="fullName"
@@ -68,9 +68,47 @@ function ContactUs(props) {
         </p>
 
         <p>
-          <input type="submit" name="submit" />
+          <input type="submit" value="Get in touch" className="submit-button" />
         </p>
       </form>
+  </section>*/
+    <section id="contact">
+      <div className="background">
+      <p classNameName="lead">
+            Feel free to contact me for any work or suggestions below
+          </p>
+        <form onSubmit={handleSubmit}>
+          <label>Name</label>
+          <input
+            className="input"
+            required
+            type="text"
+            name="fullName"
+            value={creds.fullName}
+            onChange={handleChange}
+            placeholder="Full Name"
+          />
+          <label>Email</label>
+          <input
+            required
+            type="text"
+            name="email"
+            value={creds.email}
+            onChange={handleChange}
+            placeholder="Email"
+          />
+          <label>Message</label>
+          <textarea
+            required
+            name="message"
+            rows="5"
+            placeholder="Message"
+            value={creds.message}
+            onChange={handleChange}
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
     </section>
   );
 }
